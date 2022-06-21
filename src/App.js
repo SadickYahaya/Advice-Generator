790c7ebf2594b3a3188dcee3921177862e4d7739
Sadick Abubakar Yahaya <abubakar.yahaya@amalitech.org>
2022-06-21 22:07:31 +0000

    quote generation from api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React from 'react'
 
 import Card from './components/Card';
 import './App.css';
 
 const App = () => {
-    const [quote, setQuote] = useState('');
-
-    const quoteApi = () => {
-        let arrayOfQuotes = [];
-        try {
-            const data = axios.get('https://api.quotable.io/random');
-        } catch (error) {
-            console.log(error.message)
-        }
-    }
     return (
         <div className='app'>
             <Card />
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 import Quote from './Quote';
 import '../Styles/Card.css'
 
 const Card = () => {
+    const [quote, setQuote] = useState('');
+
+    const quoteApi = async () => {
+        let arrayOfQuotes = [];
+        try {
+            const data = await axios.get('https://api.quotable.io/random');
+            arrayOfQuotes = data.data
+        } catch (error) {
+            console.log(error.message)
+        }
+
+        try {
+            setQuote(arrayOfQuotes.content)
+        } catch (error) {
+            console.log(error.message)
+        }
+    };
+
+    useEffect(() => {
+        quoteApi();
+    }, [])
+
+    const randomNumber = (number) => {
+        number = Math.floor(Math.random() * 999);
+        return number;
+    }
+
     return (
         <div className='card'>
-            <h5>Advice #117</h5>
-            <Quote quote = 'It is easy to sit up and take notice, whats difficult is getting up and taking action'/>
+            <h5>Advice #{randomNumber()}</h5>
+            <Quote quote = {quote}/>
             <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
             <div className='dice'>
-            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
+            <button onClick={quoteApi}><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg></button>
             </div>
         </div>
     )
